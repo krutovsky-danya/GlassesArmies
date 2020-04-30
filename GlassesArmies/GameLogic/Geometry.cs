bfea83d359428aced229643b912f2d56c1f42e1d
Teewar <61653747+teewar2@users.noreply.github.com>
2020-04-30 21:06:10 +0500

    add Geometry class

---
+++ GlassesArmies/GameLogic/Geometry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+
+namespace GlassesArmies
+{
+    public static class Geometry
+    {
+        public static bool CheckRectangleIntersection(Rectangle r1, Rectangle r2)
+        {
+            return Math.Max(r1.Top, r2.Top) <= Math.Min(r1.Bottom, r2.Bottom)
+                   && Math.Min(r1.Right, r2.Right) >= Math.Max(r1.Left, r2.Left);
+        }
+    }
+}
