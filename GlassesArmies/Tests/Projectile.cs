93287b05b2a4e1b32fe56e9fc6241a6cb7a24fc1
Teewar <61653747+teewar2@users.noreply.github.com>
2020-05-01 16:37:13 +0500

    add Vector and Projectile tests

---
+++ GlassesArmies/Tests/Projectile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace GlassesArmies.Tests
+{
+    [TestFixture]
+    public class ProjectileTest
+    {
+        private readonly Random _random = new Random();
+        
+        private Vector GenerateVector()
+        {
+            var x = _random.NextDouble();
+            var y = _random.NextDouble();
+            return new Vector(x, y);
+        }
+        
+        [Test]
+        public void ProjectileInitTest()
+        {
+            var location = GenerateVector();
+            var velocity = GenerateVector();
+            var projectile = new Projectile(location, velocity);
+            Assert.AreEqual( location, projectile.Location);
+            Assert.AreEqual(velocity, projectile.Velocity);
+            Assert.AreEqual(100, projectile.Live);
+        }
+        
+        [Test]
+        public void ProjectileEqualityTest()
+        {
+            var location = GenerateVector();
+            var velocity = GenerateVector();
+            var a = new Projectile(location, velocity);
+            var b = new Projectile(location, velocity);
+            Assert.AreEqual(a, b);
+        }
+
+        [Test]
+        public void ProjectileMoveTest()
+        {
+            var location = GenerateVector();
+            var velocity = GenerateVector();
+            var projectile = new Projectile(location, velocity);
+            projectile.Move();
+            Assert.AreEqual(location + velocity, projectile.Location);
+            Assert.AreEqual(99, projectile.Live);
+        }
+        
+    }
+}
--- GlassesArmies/Tests/Vector.cs
+++ GlassesArmies/Tests/Vector.cs
@@ -2,6 +2,8 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Reflection;
+using NUnit.Framework.Interfaces;
 
 namespace GlassesArmies.Tests
 {
@@ -9,8 +11,16 @@ namespace GlassesArmies.Tests
     public class VectorTests
     {
         private readonly Random _random = new Random();
+
+        private Vector GenerateVector()
+        {
+            var x = _random.NextDouble();
+            var y = _random.NextDouble();
+            return new Vector(x, y);
+        }
+        
         [Test]
-        public void VectorTestInit()
+        public void VectorInitTest()
         {
             var x = _random.NextDouble();
             var y = _random.NextDouble();
@@ -36,6 +46,7 @@ namespace GlassesArmies.Tests
             var vector = new Vector(x, y);
             var copy = vector.Copy;
             Assert.IsFalse(ReferenceEquals(vector, copy));
+            Assert.AreEqual(vector, copy);
         }
         
         [Test]
@@ -48,5 +59,26 @@ namespace GlassesArmies.Tests
             Assert.AreEqual(a, b);
         }
 
+        public void VectorArithmeticTest()
+        {
+            var a = GenerateVector();
+            var b = GenerateVector();
+            var k = _random.NextDouble();
+            var sum = a + b;
+            var dif = a - b;
+            var lExtended = k * a;
+            var rExtended = a * k;
+            var reversed = -a;
+            Assert.AreEqual(a.X + b.X, sum.X);
+            Assert.AreEqual(a.Y + b.Y, sum.Y);
+            Assert.AreEqual(a.X - b.X, dif.X);
+            Assert.AreEqual(a.Y - b.Y, dif.Y);
+            Assert.AreEqual(k * a.X, lExtended.X);
+            Assert.AreEqual(k * a.Y, lExtended.Y);
+            Assert.AreEqual(a.X * k, rExtended.X);
+            Assert.AreEqual(a.Y * k, rExtended.Y);
+            Assert.AreEqual(-a.X, reversed.X);
+            Assert.AreEqual(-a.Y, reversed.Y);
+        }
     }
 }
